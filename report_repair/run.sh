f60a981cd87bb9f4cf7006f1c64abf642abe197d
Pedro Schneider <pedroschneider@outlook.com>
2020-12-01 21:46:34 -0300

    :sparkles: :sparkles:

--- report_repair/run.sh
+++ report_repair/run.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 echo -e "part one: "
-gcc -O3 two_pointers.c -o two_pointers && ./two_pointers < 0.in && rm two_pointers
+gcc -O3 two_pointers.c -o two_pointers && ./two_pointers < 0.in
 
 echo -e "part two: "
-gcc -O3 horse.c -o horse && ./horse < 0.in && rm horse
+gcc -O3 horse.c -o horse && ./horse < 0.in
+
+# clean
+rm two_pointers
+rm horse
--- report_repair/two_pointers.c
+++ report_repair/two_pointers.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int comp(const void * a, const void * b) {
-  return ( *(int*)a - *(int*)b );
-}
+int comp(const void *a, const void *b) { return *(int *)a - *(int *)b; }
 
 int main() {
-  int n; scanf("%d", &n);
-  int *vet = (int*) malloc(sizeof(int) * n);
+  int n;
+  scanf("%d", &n);
+  int *vet = (int *)malloc(sizeof(int) * n);
 
-  int num;
-  for (int i = 0; scanf("%d", &num) == 1; ++i) {
+  for (int num, i = 0; scanf("%d", &num) == 1; ++i)
     vet[i] = num;
-  }
 
   qsort(vet, n, sizeof(int), comp);
 
@@ -20,16 +17,12 @@ int main() {
   while (left <= right) {
     int sum = vet[left] + vet[right];
 
-    if (sum == 2020) {
-      int a = vet[left];
-      int b = vet[right];
-      printf("%d * %d = %d\n", a, b, a * b);
-      return 0;
-    } else if (sum < 2020) {
+    if (sum == 2020)
+      return printf("%d\n", vet[left] * vet[right]);
+    else if (sum < 2020)
       left++;
-    } else if (sum > 2020) {
+    else if (sum > 2020)
       right--;
-    } 
   }
 
   return -1;
