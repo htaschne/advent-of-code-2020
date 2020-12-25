f2f380c9ec6ac376d14bfcb09654d18308c61ab2
Pedro Schneider <pedroschneider@outlook.com>
2020-12-25 01:30:08 -0300

    :sparkles: :sparkles:

--- day22-crab_combat/combat.py
+++ day22-crab_combat/combat.py
@@ -1,4 +1,3 @@
-
 import sys
 
 from collections import deque
@@ -8,8 +7,7 @@ p1, p2 = open(sys.argv[1]).read().strip().split("\n\n")
 p1 = deque(list(map(int, [x.rstrip() for x in p1.split("\n")][1:])))
 p2 = deque(list(map(int, [x.rstrip() for x in p2.split("\n")][1:])))
 
-def simulate(p1, p2):
-
+def regular_combat(p1, p2):
   n = 1
   while True:
     # print("-- Round %d --" % (n+1))
@@ -39,8 +37,52 @@ def simulate(p1, p2):
     n += 1
 
 
-ret = simulate(p1, p2)
+ret = regular_combat(p1.copy(), p2.copy())
 acc = 0
 for i in range(len(ret)):
   acc += ret[i] * (len(ret) - i)
+print(acc)
+
+
+def recursive_combat(deck1, deck2):
+  rounds = [] # this should really be a set but whatever
+
+  while len(deck1) != 0 and len(deck2) != 0:
+    if (deck1, deck2) in rounds:
+      return True
+
+    rounds.append((deck1.copy(), deck2.copy()))
+
+    c1 = deck1.pop(0)
+    c2 = deck2.pop(0)
+
+    # determine winner with a sub game
+    if len(deck1) >= c1 and len(deck2) >= c2:
+      winner = recursive_combat(deck1.copy()[:c1], deck2.copy()[:c2])
+
+      if winner:
+          deck1.append(c1)
+          deck1.append(c2)
+      else:
+          deck2.append(c2)
+          deck2.append(c1)
+
+    # determine winner by card value
+    else:
+      if c1 > c2:
+          deck1.append(c1)
+          deck1.append(c2)
+      elif c2 > c1:
+          deck2.append(c2)
+          deck2.append(c1)
+
+  return len(deck1) != 0
+
+d1 = [x for x in p1]
+d2 = [x for x in p2]
+ret = recursive_combat(d1, d2)
+dd = d1 if ret else d2
+acc = 0
+for i in range(len(dd)):
+  acc += dd[i] * (len(dd) - i)
 print(acc)
